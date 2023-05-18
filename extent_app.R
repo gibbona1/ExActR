de5aedcf10511911447fd292b039b9aa7cc46f88
gibbona1 <33295389+gibbona1@users.noreply.github.com>
2023-05-18 16:36:15 +0100

    upload shapefile functionality

--- extent_app.R
+++ extent_app.R
@@ -2,64 +2,113 @@ library(shiny)
 library(leaflet)
 library(sf)
 
+map_accepts <- c('.shp','.dbf','.sbn','.sbx','.shx',".prj")
+
 shinyApp(
   ui = fluidPage(
-    column(5,
-           h5("Opening Map"),
-           leafletOutput("plot1")
+    fluidRow(
+      column(4, fileInput("sf1", "Upload Opening Map", accept=map_accepts, multiple = TRUE)),
+      column(4, align='center', br(), actionButton("extent", "Generate Extent")),
+      column(4, fileInput("sf2", "Upload Closing Map", accept=map_accepts, multiple = TRUE))
     ),
-    column(2,
-           h5(""), br(),
-           actionButton("extent", "Generate Extent Account")
-    ),
-    column(5,
-           h5("Closing Map"),
-           leafletOutput("plot2")
+    fluidRow(
+      column(6,
+             h3("Opening Map"),
+             leafletOutput("plot1")
+      ),
+      column(6,
+             h3("Closing Map"),
+             leafletOutput("plot2")
+      )
     )
   ),
-  server = function(input, output) {
+  server <- function(input, output) {
+    
+    setup_read_sf <- function(shpdf){
+      previouswd <- getwd()
+      uploaddirectory <- dirname(shpdf$datapath[1])
+      setwd(uploaddirectory)
+      for(i in 1:nrow(shpdf)){
+        file.rename(shpdf$datapath[i], shpdf$name[i])
+      }
+      setwd(previouswd)
+      
+      tmp_file1 <- paste(uploaddirectory, shpdf$name[grep(pattern="*.shp$", shpdf$name)], sep="/")
+      map  <- rgdal::readOGR(tmp_file1)
+      #map2 <- rgdal::spTransform(map, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
+      
+      return(map)
+    }
     # Read shapefiles
-    sf1 <- st_read("Hazelwood/hazelwood_CLC2000.shp", quiet=TRUE)
-    sf2 <- st_read("Hazelwood/hazelwood_CLC2018.shp", quiet=TRUE)
+    sf1 <- reactive({
+      #tmp_file1 <- "Hazelwood/hazelwood_CLC2000.shp"
+      #if(is.null(input$sf1)){
+        #showNotification(HTML(paste0("Using temporary file ", tags$b(tmp_file1), ".")),
+        #                 duration = NULL, type = "message")        
+        #return(read_sf(tmp_file1, quiet=TRUE))
+      #  showNotification(HTML("No file uploaded, leaving blank."),
+      #                   duration = NULL, type = "message")
+      #  return(NULL)
+      #}
+      req(input$sf1)
+      return(setup_read_sf(input$sf1))
+    })
     
-    habitat_palette1 <- colorFactor(
-        palette = "viridis",
-        domain = unique(sf1$CODE_00)
-      )
-    
-    habitat_palette2 <- colorFactor(
-      palette = "viridis",
-      domain = unique(sf1$CODE_18)
-    )
+    sf2 <- reactive({
+      req(input$sf2)
+      return(setup_read_sf(input$sf2))
+    })
     
     # Render the first plot
     output$plot1 <- renderLeaflet({
+      if(is.null(input$sf1)){
+        return(leaflet() %>%
+                 #addProviderTiles("OpenStreetMap.BlackAndWhite") %>%
+                 setView(lng = 0, lat = 0, zoom = 2))
+      }
+      
+      habitat_palette1 <- colorFactor(
+        palette = "viridis",
+        domain = unique(sf1()$CODE_00)
+      )
+      
       leaflet() %>%
         addTiles() %>%
-        addPolygons(data = sf1,
-                    fillColor = habitat_palette1(sf1$CODE_00),
+        addPolygons(data = sf1(),
+                    fillColor = habitat_palette1(sf1()$CODE_00),
                     fillOpacity = 0.7,
                     color = "#b2aeae", #boundary colour, need to use hex color codes.
                     weight = 0.5, 
                     smoothFactor = 0.2) %>%
         addLegend(pal = habitat_palette1,
-                  values = sf1$CODE_00, 
+                  values = sf1()$CODE_00, 
                   position = "bottomleft", 
                   title = "Code <br>")
     })
     
     # Render the second plot
     output$plot2 <- renderLeaflet({
+      if(is.null(input$sf2)){
+        return(leaflet() %>%
+                 #addProviderTiles("OpenStreetMap.BlackAndWhite") %>%
+                 setView(lng = 10, lat = 10, zoom = 2))
+      }
+      
+      habitat_palette2 <- colorFactor(
+        palette = "viridis",
+        domain = unique(sf2()$CODE_18)
+      )
+      
       leaflet() %>%
         addTiles() %>%
-        addPolygons(data = sf2,
-                    fillColor = habitat_palette2(sf2$CODE_18),
+        addPolygons(data = sf2(),
+                    fillColor = habitat_palette2(sf2()$CODE_18),
                     fillOpacity = 0.7,
                     color = "#b2aeae", #boundary colour, need to use hex color codes.
                     weight = 0.5, 
                     smoothFactor = 0.2) %>%
         addLegend(pal = habitat_palette2,
-                  values = sf2$CODE_18, 
+                  values = sf2()$CODE_18, 
                   position = "bottomleft", 
                   title = "Code <br>")
     })
