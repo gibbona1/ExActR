e5314e5f3aa621d5d6a6196d1e88141543087055
gibbona1 <33295389+gibbona1@users.noreply.github.com>
2023-07-31 13:11:35 +0100

    fix alert for success/error in copying tables

--- www/copytable.js
+++ www/copytable.js
@@ -20,7 +20,7 @@ function copytable(el, format) {
         alert('Table copied successfully as ' + format);
       })
       .catch(function(error) {
-        alert('Unable to copy table: ', error);
+        alert('Unable to copy table: ' + error);
       });
   }
   window.getSelection().removeAllRanges();
@@ -74,14 +74,22 @@ function convertTableToLaTeX(tablediv) {
 }
 
 function copyToClipboard(text) {
-    var dummy = document.createElement("textarea");
-    // to avoid breaking orgain page when copying more words
-    // cant copy when adding below this code
-    // dummy.style.display = 'none'
-    document.body.appendChild(dummy);
-    //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
-    dummy.value = text;
-    dummy.select();
-    document.execCommand("copy");
-    document.body.removeChild(dummy);
+  return new Promise(function(resolve, reject) {
+    
+    try {
+      var dummy = document.createElement("textarea");
+      // to avoid breaking orgain page when copying more words
+      // cant copy when adding below this code
+      // dummy.style.display = 'none'
+      document.body.appendChild(dummy);
+      //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
+      dummy.value = text;
+      dummy.select();
+      document.execCommand("copy");
+      document.body.removeChild(dummy);
+      resolve();
+    } catch(error) {
+      reject(error);
+    }
+  })
 }
